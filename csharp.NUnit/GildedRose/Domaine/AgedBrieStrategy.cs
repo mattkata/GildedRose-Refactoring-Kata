1b2c15a26ab8d431043bc878017248c80e3d678a
mattkata <matthieu.dolez.ca@gmail.com>
2024-12-17 21:10:37 +0100

    ajout des strategies de mise a jour pour le brie et les billets de concert

--- csharp.NUnit/GildedRose/Domaine/AgedBrieStrategy.cs
+++ csharp.NUnit/GildedRose/Domaine/AgedBrieStrategy.cs
@@ -4,7 +4,12 @@
     public class AgedBrieStrategy : IUpdateStrategy
     {
         public void Update(Item item) {
-            
+            // La qualité du brie augmente avec le temps
+            // Augmente de 1 par jour. Augmente de 2 après expiration.
+            // Limite à 50.
+            item.Quality = Math.Min(50, item.Quality + (item.SellIn >  0 ? 1 : 2));
+            // La date diminue de 1 par jour
+            item.SellIn--;
         }
     }
 }
--- csharp.NUnit/GildedRose/Domaine/BackstagePassStrategy.cs
+++ csharp.NUnit/GildedRose/Domaine/BackstagePassStrategy.cs
@@ -1,11 +1,36 @@
-﻿namespace GildedRoseKata.Domaine
+﻿using System;
+
+namespace GildedRoseKata.Domaine
 {
     // Stratégie pour les objets billets de concert
     public class BackstagePassStrategy : IUpdateStrategy
     {
         public void Update(Item item)
         {
-            
+            // La qualité des billets augmente avec le temps sauf si le concert a eu lieu
+            // Augmente de 1 par jour si il y a plus de 10 jours restant -- limite a 50
+            // Augmente de 2 entre 6 et 10 jours restant -- limite a 50
+            // Augmente de 3 entre 1 et 5 jours restant -- limite a 50
+            // la qualité tombe a 0 si le concert a eu lieu
+            if (item.SellIn <= 0)
+            {
+                item.Quality = 0; 
+            }
+            else if (item.SellIn <= 5)
+            {
+                item.Quality = Math.Min(50, item.Quality + 3);
+            }
+            else if (item.Quality <= 10)
+            {
+                item.Quality = Math.Min(50, item.Quality + 2);
+            }
+            else
+            {
+                item.Quality = Math.Min(50, item.Quality + 1);
+            }
+
+            // La date diminue de 1 par jour
+            item.SellIn--;
         }
     }
 }
